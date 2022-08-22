4fccc1504608af002fc658e0e178e2701f15a083
Mykhailo <misha333211@gmail.com>
2022-08-22 16:27:23 +0300

    Kokos

---
+++ src/test/java/Tests/TestKokos.java
@@ -0,0 +1,63 @@
+package Tests;
+
+import PageObjects.KokosHomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class TestKokos extends TestInit{
+
+    @Test
+    public void getXiaomi() {
+        KokosHomePage kokosHomePage = new KokosHomePage(driver);
+        Actions actions = new Actions(driver);
+
+        kokosHomePage.goToKokos();
+        actions.moveToElement(kokosHomePage.waitElementToBeVisible("//div[@class='products-menu__title']//a[@href='/ua/mobile-phones/']")).build().perform();
+        kokosHomePage.getXiaomi();
+        kokosHomePage.getRealmeBtn();
+        kokosHomePage.getSearchBtn();
+        kokosHomePage.getSearchClick();
+        kokosHomePage.getMobileBtn();
+        kokosHomePage.getPicture();
+        sleep(2);
+        kokosHomePage.getBuy();
+        kokosHomePage.getOrder();
+        driver.quit();
+
+    }
+
+    @Test
+    public void checkFilters(){
+        KokosHomePage kokosHomePage = new KokosHomePage(driver);
+        kokosHomePage.goToKokos();
+        kokosHomePage.getMenu();
+        kokosHomePage.getSamsungBtn();
+        sleep(3);
+        kokosHomePage.getGb();
+        kokosHomePage.getMinBtn();
+        kokosHomePage.getMinClear();
+        kokosHomePage.getMinSearch();
+        kokosHomePage.getSmallBtn2();
+        driver.quit();
+
+    }
+
+    @Test
+    public void WebSearch(){
+        KokosHomePage kokosHomePage = new KokosHomePage(driver);
+        kokosHomePage.goToKokos();
+        kokosHomePage.getSearchBtn2();
+        kokosHomePage.getSearchClick();
+        kokosHomePage.getSharkImg();
+        kokosHomePage.getNameSearch();
+        kokosHomePage.getEmailSearch();
+        kokosHomePage.getSMSSearch();
+        kokosHomePage.getStarsBtn();
+        sleep(1);
+        driver.quit();
+    }
+
+}
