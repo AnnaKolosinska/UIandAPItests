04644b1251d5d1e96bdad7674f9ebf69fcb46901
Mykhailo <misha333211@gmail.com>
2022-08-24 11:00:38 +0300

    OLX

---
+++ src/test/java/Tests/TestOLX.java
@@ -0,0 +1,64 @@
+package Tests;
+
+import PageObjects.OLXHomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestOLX extends TestInit{
+
+    @Test
+    public void checkOLX(){
+        OLXHomePage olxHomePage = new OLXHomePage(driver);
+        olxHomePage.goToOLX();
+        olxHomePage.getSearch();
+        sleep(1);
+        olxHomePage.getCloseBtn();
+        olxHomePage.getCity();
+        olxHomePage.getTernopil();
+        olxHomePage.getTernopilBtn();
+        olxHomePage.getMaxPrice();
+        olxHomePage.getBuyToyota();
+        olxHomePage.getSwipe();
+        sleep(1);
+        olxHomePage.getSwipe2();
+        driver.quit();
+    }
+
+    @Test
+    public void OLXPrice(){
+        OLXHomePage olxHomePage = new OLXHomePage(driver);
+        olxHomePage.goToCamry();
+        olxHomePage.getCloseBtn();
+        olxHomePage.getOlegBtn();
+        olxHomePage.getWork();
+        sleep(1);
+        olxHomePage.getLogo();
+        olxHomePage.getHouse();
+        sleep(1);
+        olxHomePage.getRealty();
+        olxHomePage.getFlat();
+        olxHomePage.getPhoto();
+        olxHomePage.getDelivery();
+        olxHomePage.getWorkingNow();
+        sleep(1);
+        driver.quit();
+
+    }
+    @Test
+    public void AssertCheck(){
+        OLXHomePage olxHomePage = new OLXHomePage(driver);
+        olxHomePage.goToCamry();
+        Assert.assertTrue(olxHomePage.getCloseBtn2().isDisplayed());
+        Assert.assertTrue(olxHomePage.getSearch2().isDisplayed());
+        Assert.assertTrue(olxHomePage.getCity2().isDisplayed());
+        Assert.assertTrue(olxHomePage.getSearchBtn2().isDisplayed());
+        Assert.assertTrue(olxHomePage.getBack().isDisplayed());
+        Assert.assertTrue(olxHomePage.getOleg().isDisplayed());
+        Assert.assertTrue(olxHomePage.getWhatIs().isDisplayed());
+        Assert.assertTrue(olxHomePage.getPicture2().isDisplayed());
+        Assert.assertTrue(olxHomePage.getPrice().isDisplayed());
+        Assert.assertTrue(olxHomePage.getPrivate().isDisplayed());
+
+    }
+
+}
